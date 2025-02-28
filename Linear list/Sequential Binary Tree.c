04d300aaf7f9c4f0471c1ad5fa01ef807b36c8bb
xin-xin-hub <1332579773@qq.com>
2025-03-01 00:10:49 +0800

    Add files via upload

---
+++ Linear list/Sequential Binary Tree.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAXSIZE 100
+#define int ElemType
+typedef ElemType SqBiTree[MAXSIZE]
+SqBiTree bt;//浪费空间，适于存满二叉树和完全二叉树
+			//顺序二叉树第n个元素的左子节点为2*n+1
+			//顺序二叉树第n个元素的右子节点为2*n+2
+			//顺序二叉树第n个元素的父节点为(n-1)/2
+
+int main() {//test
+
+    return 0;
+
+} 

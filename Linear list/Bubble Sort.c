7db64b148ef22c5db88def1ad02ffd7f530fa2dd
xin-xin-hub <1332579773@qq.com>
2025-03-25 00:18:13 +0800

    Add files via upload

--- Linear list/Bubble Sort.c
+++ Linear list/Bubble Sort.c
@@ -16,11 +16,14 @@ typedef struct{//定义顺序表的结构
 }SqList; 
 
 void bubble_sort(SqList *L){
+	int flag = 1;///flag作为是否有交换的标记 
 	int i,j;
 	RedType x;//交换时临时存储
-	for(i = 1;i <= L->length-1;i++){
+	for(i = 1;i <= L->length-1 && flag == 1;i++){
+		flag = 0;
 		for(j = 1;j <= L->length - i;j++){//每一轮大数沉底 
 			if(L->r[j].key > L->r[j+1].key){//发生逆序
+				flag = 1;
 				x = L->r[j];//交换顺序 
 				L->r[j] = L->r[j+1];
 				L->r[j+1] = x;
